667a1d04f8f90ac20625b254a1c3b3e4adfb59bf
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-06-16 13:27:01 +0300

    Тип на виході з ф-ії. Виправлення рекомендовані пайчарм.

--- 1.Create dictionary.py
+++ 1.Create dictionary.py
@@ -1,13 +1,14 @@
-def create_dictionary(*args):
+def create_dictionary(*args) -> dict:
     dictionary = {}
     number = 0
     for argument in args:
-        if isinstance(argument, list) is False:
+        if not isinstance(argument, list):
             dictionary[argument] = number
         else:
             print(f"Cannot add {argument} to the dict!")
         number += 1
-    return(dictionary)
+    return dictionary
 
 print(create_dictionary(7, 1, 3))
 print(create_dictionary(3, [1, 2], 5))
+print(create_dictionary(3, (1, 2), 5))
