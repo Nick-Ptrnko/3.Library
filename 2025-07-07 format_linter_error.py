a689bed302aad5516c8a1f5d581899d6a32b5c24
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-07-07 22:14:00 +0300

    перша функція через один рядок є!

--- 2025-07-07 format_linter_error.py
+++ 2025-07-07 format_linter_error.py
@@ -1,29 +1,3 @@
-#       ФУНКЦІЯ ЗАМІНЯЄ ОДНЕ ЗНАЧЕННЯ КЛЮЧА НА ІНШЕ
-#      ЯКЩО ЗНАЧЕННЯ ЯКЕ ПРИХОДИТЬ ВІДСУТНЄ, ВОНА ПОВЕРНЕ None
-def Changing_a_key(key: str) -> str:
-    if key == "code":
-        return "name"
-    if key == "line_number":
-        return "line"
-    if key == "column_number":
-        return "column"
-    if key == "text":
-        return "message"
-    return None
-
-def format_linter_error(error: dict) -> dict:
-    temp_data = {Changing_a_key(key): value
-                 for key, value in error.items()
-                 if Changing_a_key(key) is not None}
-    # Розпаковуємо в потрібній послідовності
-    return {
-        "line": temp_data["line"],
-        "column": temp_data["column"],
-        "message": temp_data["message"],
-        "name": temp_data["name"],
-        "source": "flake8"  # Додаємо новий елемент
-    }
-
 
 error = {
     "code": "E501",
@@ -34,5 +8,14 @@ error = {
     "physical_line": '    return f"I like to filter, rounding, doubling, '
                      "store and decorate numbers: {', '.join(items)}!\"",
 }
+
+def format_linter_error(error: dict) -> dict:
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 print(format_linter_error(error=error))
