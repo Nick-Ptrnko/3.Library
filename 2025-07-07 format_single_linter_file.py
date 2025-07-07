20ae4c1a2d3c9006de5b904e55c9055d75862733
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-07-07 22:33:51 +0300

    Початок написання другої функції

---
+++ 2025-07-07 format_single_linter_file.py
@@ -0,0 +1,25 @@
+errors = [
+    {
+        "code": "E501",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 80,
+         "text": "line too long (99 > 79 characters)",
+        "physical_line": '    return f"I like to filter, rounding, doubling, ' 
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+    {
+        "code": "W292",
+        "filename": "./source_code_2.py",
+        "line_number": 18,
+        "column_number": 100,
+        "text": "no newline at end of file",
+        "physical_line": '    return f"I like to filter, rounding, doubling, '
+        "store and decorate numbers: {', '.join(items)}!\"",
+    },
+]
+
+def format_single_linter_file(file_path: str, errors: list) -> dict:
+    return {"errors":[], "path": file_path, "status": "failed"}
+
+print(format_single_linter_file(file_path="./source_code_2.py", errors=errors))
