e354dcf7109c1b8d322c86f8023cb4f7648d1e26
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-07-15 15:08:02 +0300

    Виклик функції з функції

--- 2025-07-14 Виклик функції із функції.py
+++ 2025-07-14 Виклик функції із функції.py
@@ -1,6 +1,7 @@
 def new_number(number: int) -> None:
     print(f"Received a new number: {number}")
 
+
 def is_positive(number: int) -> None:
     if number > 0:
         print(f"{number} is a positive number")
@@ -10,11 +11,12 @@ def is_positive(number: int) -> None:
         print(f"{number} is a negative number")
     pass
 
+
 def print_bye(number: int) -> None:
     print("Bye!")
 
 
-def numbers_handler(numbers: list, before=new_number, action=is_positive, after=print_bye,) -> None:
+def numbers_handler(numbers: list, before: callable = new_number, action: callable = is_positive, after: callable = print_bye) -> None:
     for number in numbers:
         before(number)
         action(number)
