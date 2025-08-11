89086593ac24e7dfa2612ca4ba8391f4fbc5483e
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-08-11 06:51:14 +0300

    2025-08-11

--- split_func/string_split.py
+++ split_func/string_split.py
@@ -3,9 +3,9 @@
 та повертає суму всіх елементів цих списків
 '''
 def get_lists_sum(list_1, list_2):
-    sum_two_list = 0
+    sum_two_list = []
     for i in range(len(list_1)):
-        sum_two_list += int(list_1[i]) + int(list_2[i])
+        sum_two_list.append(int(list_1[i]) + int(list_2[i]))
     return sum_two_list
 my_string1 = input("Введіть перший список \n")
 my_list1 = my_string1.split(", ")
