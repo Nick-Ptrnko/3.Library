4a5aebf392c0569c432fb3eea2a1d5882d8df843
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-09-18 22:45:50 +0300

    decorator

--- decorator.py
+++ decorator.py
@@ -1,12 +1,20 @@
-def decorate(func: callable):
+def decorate(func: callable): #Функція-декоратор
     def inner():
         print("Decorate before")
         func()
         print("Decorate after")
     return inner
 
-def printer():
+'''def printer(): #Функція, яку декоруємо
     print("Hello")
 
-decorated_printer = decorate(printer)
-decorated_printer()
+decorated_printer = decorate(printer) #Присвоєння змінній decorated_printer результату виконання функції inner зі змінною printer
+decorated_printer() #Нова функція (по суті задекорована функція printer())'''
+
+#Рядок 11 можна замінити рядком 15:
+@decorate
+def printer(): #Функція, яку декоруємо
+    print("Hello")
+
+printer() #Тепер викликаючи функцію за її попереднім ім'ям, отримуємо нову - задекоровану функцію
+
