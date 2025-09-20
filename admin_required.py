d0abb597699b59f7ec72ed91ab9ccd4b60fd359d
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-09-20 13:04:09 +0300

    2025-09-20

--- admin_required.py
+++ admin_required.py
@@ -1,11 +1,29 @@
-def repeat(times, func):
-    def wrapper(*args, **kwargs):
-        for _ in range(times):
-            func(*args, **kwargs)
-    return wrapper
+'''
+Дана функція send_secure_information, яка приймає словник user, вона повідомляє користувачу
+деяку секретну інформацію.
+Напиши декоратор admin_required, який перевіряє атрибут user'a 'is_admin'
+і якщо він False - виводить повідомлення з забороною, як показано у прикладі.
+'''
+from typing import Callable
 
-@repeat(4)
-def say(message):
-    print(message)
+def send_secure_information(user: dict) -> None:
+    print(f"{user['name']}, site's secure code is 'SecUR43Esit78Eco90DE'")
 
-say("Python")
+
+def admin_required(func: Callable) -> None:
+    # write your code here
+    pass
+
+
+#Приклад:
+@admin_required
+def send_secure_information(user: dict) -> None:
+        print(f"{user['name']}, site's secure code is 'SecUR43Esit78Eco90DE'")
+
+send_secure_information({'name': 'Administrator', 'is_admin': True})
+# Administrator, site's secure code is 'SecUR43Esit78Eco90DE'
+
+send_secure_information({'name': 'User1234', 'is_admin': False})
+# You are not allowed to see this information!
+
+
