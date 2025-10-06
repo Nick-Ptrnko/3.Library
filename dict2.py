d310d94bd909098e3fde537718f302a3224313cb
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-10-06 22:36:02 +0300

    2025-10-06

---
+++ dict2.py
@@ -0,0 +1,7 @@
+my_dict = {"name": "Alex", "age": 20}
+print(my_dict)
+my_dict["age"] = 21 
+my_dict["specialization"] = "Computer Science"
+print(my_dict)
+del my_dict["specialization"]
+print(my_dict)
