518860c28cfc0ecda3690e47d2bbb9c5c7d6d4be
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-10-22 13:42:41 +0300

    rock_band

---
+++ classes/class_method.py
@@ -0,0 +1,4 @@
+class Magic:
+    def __init__(self):
+        print("Hello, I'm magic method!")
+print(dir(Magic)) # ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', … ]
---
+++ classes/rock_band.py
@@ -0,0 +1,56 @@
+'''
+Реалізуй клас RockBand, який зберігатиме інформацію про музичну групу.
+Метод __init__ класу повинен приймати два параметри та зберегти їх у відповідні атрибути:
+name - назва групи
+members - список учасників групи
+Приклад ініціалізації екземпляра цього класу:
+
+the_beatles = RockBand(
+  "The Beatles",
+  ["John Lennon", "Paul McCartney", "George Harrison"]
+)
+
+У класі RockBand має бути метод add_new_member, який приймає рядок new_member - нового учасника
+групи. Якщо такий учасник з таким ім'ям вже є, то метод має виводити рядок про помилку у форматі
+{new_member} is already in the band!. Інакше учасник має бути доданий до списку members
+
+the_beatles.add_new_member("Ringo Starr")
+the_beatles.add_new_member("John Lennon") # 'John Lennon already in the band'
+
+Групи часто об'єднуються в одну, і клас RockBand теж повинен підтримувати цю можливість.
+Реалізуйте магічний метод __add__. В результаті складання двох груп має виходити група:
+назва якої дорівнює {name1} {name2} United, де name1 - це назва першої групи, а name2 - назва другої групи
+а список учасників отримується шляхом об'єднання учасників обох груп. Зверніть увагу, що якщо
+учасник є в обох групах до об'єднання, він повинен бути вказаний в об'єднаній групі лише один раз!
+
+first_band = RockBand("First", ["Ivan", "Sergey"])
+second_band = RockBand("Second", ["Sergey", "Max"])
+united = first_band + second_band
+print(united.name, united.members) # "First Second United" ["Ivan", "Sergey", "Max"]
+
+'''
+
+class RockBand:
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+    def add_new_member(self, new_member: str):
+        if new_member in self.members:
+            print(f"{new_member} is already in the band!")
+        else:
+            self.members.append(new_member)
+    def __add__(self, other):
+        united_group = set(self.members + other.members)
+        return RockBand(f"{self.name} {other.name} United", list(united_group))
+the_beatles = RockBand(
+  "The Beatles",
+  ["John Lennon", "Paul McCartney", "George Harrison"]
+)
+print(the_beatles.name, the_beatles.members)
+the_beatles.add_new_member("Ringo Starr")
+print(the_beatles.name, the_beatles.members)
+the_beatles.add_new_member("John Lennon")
+first_band = RockBand("First", ["Ivan", "Sergey"])
+second_band = RockBand("Second", ["Sergey", "Max"])
+united = first_band + second_band
+print(united.name, united.members) # "First Second United" ["Ivan", "Sergey", "Max"]
