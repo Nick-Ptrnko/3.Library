2104c1e93bc3a6f4668c0f017fd330524a7198d0
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-11-07 16:46:54 +0200

    2025-11-07

---
+++ Generators/time_range.py
@@ -0,0 +1,42 @@
+'''
+Напиши генератор time_range, який приймає два аргументи, час початку та час закінчення: time_start, time_end
+- кортежі з трьома цілими hours, minutes, seconds.
+На першій ітерації time_range повинен повертати початковий час time_start, на кожній наступній ітерації він повертає
+попереднє значення, збільшене на одну секунду.
+Приклад:
+t_range = time_range(time_start=(10, 0, 0),
+                     time_end=(10, 0, 3))
+next(t_range) == (10, 0, 0)
+next(t_range) == (10, 0, 1)
+next(t_range) == (10, 0, 2)
+next(t_range)
+# Error: StopIteration
+
+Час закінчення може бути меншим за початковий.
+t_range = time_range(time_start=(23, 59, 59),
+                     time_end=(0, 0, 3))
+t_range_list = list(t_range)
+t_range_list == [
+  (23, 59, 59),
+  (0, 0, 0),
+  (0, 0, 1),
+  (0, 0, 2)
+]
+'''
+
+def time_range(time_start: tuple, time_end: tuple) -> tuple:
+    hours, minutes, seconds = time_start
+    yield hours, minutes, seconds
+    if seconds >= 60:
+        seconds = 0
+        minutes += 1
+    elif minutes >= 60:
+        minutes = 0
+        hours += 1
+    elif hours >= 24:
+        hours = 0
+    seconds += 1
+    yield hours, minutes, seconds
+
+t_range = time_range(time_start=(10, 0, 0),
+                     time_end=(10, 0, 3))
