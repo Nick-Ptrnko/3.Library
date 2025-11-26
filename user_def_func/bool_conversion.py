6b6e3b6d4aa4960e052e486d724fc268b4c5f71f
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-11-26 21:54:23 +0200

    2025-11-26

--- user_def_func/bool_conversion.py
+++ user_def_func/bool_conversion.py
@@ -38,3 +38,29 @@ bool_value = make_bool({True})
 
 Виняток BoolConversionError має наслідуватись лише від класу Exception.
 """
+class BoolConversionError(Exception):
+    pass
+
+
+def from_int(value: int) -> bool:
+    if not isinstance(value, int):
+        raise TypeError
+    if value == 1:
+        return True
+    elif value == 0:
+        return False
+    else:
+        raise ValueError
+
+
+def from_str(value: str) -> bool:
+    if not isinstance(value, str):
+        raise TypeError
+    if value in ["True", "T", "1"]:
+        return True
+    elif value in ["False", "F", "0"]:
+        return False
+    else:
+        raise ValueError
+
+def make_bool(value: int) -> bool:
